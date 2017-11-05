b04bba1c21895f4d784b201f843d4c1a39d1b91c
ledet <ledet2016@gmail.com>
2017-11-05 00:49:00 -0500

    file precedence

--- webApp/Medilab/js/controller.js
+++ webApp/Medilab/js/controller.js
@@ -3,14 +3,16 @@ client.on("connect", function() {
     console.log('connected');
     client.subscribe('distrk/patient/1');
 });
+
+
 client.on('message', function(topic, message) {
 
     console.log(JSON.parse(message.toString()));
-    var json = JSON.parse(message.toString());
+     json = JSON.parse(message.toString());
 
-    var id = json.id;
-    var name = json.name;
-    var completed = json.completed;
+     window.id = json.id;
+     name = json.name;
+     completed = json.completed;
     var deparments_id = json.deparments[0].id;
     var deparments_name = json.deparments[0].name;
     var deparments_location = json.deparments[0].location;
