932c3e48f9769a89fa265f8856a64cf65b83117b
ledet <ledet2016@gmail.com>
2017-11-05 01:58:25 -0500

    functionality that connects with database

--- webApp/Medilab/js/controller.js
+++ webApp/Medilab/js/controller.js
@@ -9,14 +9,35 @@ client.on('message', function(topic, message) {
 
     console.log(JSON.parse(message.toString()));
      json = JSON.parse(message.toString());
+	 var deparments_id = json.deparments[0].id;
 
+	/*
      window.id = json.id;
      name = json.name;
      completed = json.completed;
     var deparments_id = json.deparments[0].id;
     var deparments_name = json.deparments[0].name;
     var deparments_location = json.deparments[0].location;
-
-    w3.displayObject("id01", json);
+	
+	for each (variable in object) {
+  statement
+}
+*/
+  //  w3.displayObject("id0100", json);
+	w3.displayObject("id0200", json);
+	w3.displayObject("id0300", json);
+	//w3.displayObject("id04", json);
+	//w3.displayObject("id05", json);
+	//w3.displayObject("id06", json);
+	
+	console.log(json.deparments.length);
+	
+	for(var i = 0; i < json.deparments.length; i++ ) {
+		var value = (i + 1);
+		w3.displayObject(value, json );
+		console.log(i);
+		console.log(value);
+	}
+	
 });
 
